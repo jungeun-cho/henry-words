107aff4caedf9ea3873323b0c35b3361bb125787
jenny-cho <jenny.cho@nhn.com>
2023-05-14 08:51:53 +0900

    style: 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Head from 'next/head';
 import FlipCardContainer from './components/FlipCardContainer';
 
 export default function Home() {
-  return <FlipCardContainer />
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <FlipCardContainer />
+  </>
 }
